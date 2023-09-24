58fcd91c0d9af492a2047ad01a557ca20f76b3eb
SANJIKS <sanzaramratov588@gmail.com>
2023-09-24 13:03:57 +0600

    added discussion models to admin

--- apps/discussions/admin.py
+++ apps/discussions/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Discussion, Comment, Category, Like, DisLike
 # Register your models here.
+
+admin.site.register([Discussion, Comment, Category, Like, DisLike])
